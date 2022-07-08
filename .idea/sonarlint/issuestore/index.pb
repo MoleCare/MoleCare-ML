78df1ba7394b57e2a51948b27a074a9c19f53cfc
Yauhen Bichel <5603242+YauhenBichel@users.noreply.github.com>
2022-07-08 17:34:08 +0100

    add serving

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 X
 (data/build-deploy/molecare-ml.dockerfile,7/c/7c7892090161b127f7a76c71f101f38ff9ca4199
-G
-data/docs/TensorFlow.md,2/5/251f47f4412defd3b3f614f767ad79acc8a149c4
 F
 data/docs/Run-Flask.md,2/f/2f88f7ea8ac5a8455b2b1ce2c330108fe6f062bf
 9
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, \
+    request
 from flask_cors import CORS, cross_origin
 import os
+import model
 import re
 from datetime import datetime
 
@@ -34,6 +36,59 @@ def hello_there(name):
               "! It's " + formatted_now
     return content
 
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.join('static', uploaded_file.filename)
+            uploaded_file.save(image_path)
+            class_name = model.get_prediction(image_path)
+            result = {
+                'class_name': class_name,
+                'image_path': image_path,
+            }
+            return result
+    return {'error': 'filename is empty string'}
+
+
+#r = requests.post('http://image-serving:8501/v1/models/qualitynet:predict', json=payload)
+# was "http://localhost:8501"
+@app.route('/image-quality')
+def image_quality():
+    data = {}
+
+    if not request.files["image"]:
+        return jsonify({"status": 400, "message": 'No image passed'})
+
+    # Decoding and pre-processing base64 image
+    img = image.img_to_array(image.load_img(BytesIO(request.files["image"].read()),
+                                            target_size=(150, 150))) / 255.
+
+    # this line is added because of a bug in tf_serving < 1.11
+    img = img.astype('float16')
+
+    # Creating payload for TensorFlow serving request
+    payload = {
+        "instances": [{'input_image': img.tolist()}]
+    }
+
+    # Making POST request
+    r = requests.post('http://localhost:8501/v1/models/qualitynet:predict', json=payload)
+
+    # Decoding results from TensorFlow Serving server
+    pred = json.loads(r.content.decode('utf-8'))
+
+    pred = (np.array(pred['predictions'])[0] > 0.4).astype(np.int)
+    if pred == 0:
+        prediction = 'Bad'
+    else:
+        prediction = 'Good'
+
+    data["prediction"] = prediction
+
+    # Returning JSON response
+    return jsonify({"status": 200, "message": 'No image passed', "data": data })
 
 if __name__ == "__main__":
     from waitress import serve
