0d7af5461a5f984c2f32cc205164ed5da13f2929
Yauhen Bichel <5603242+YauhenBichel@users.noreply.github.com>
2022-08-18 09:28:48 +0100

    app.py refactoring

---
+++ doc/app-with-tf-serving.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import tensorflow as tf
+import os
+import numpy as np
+from io import BytesIO
+import base64
+from PIL import Image
+
+app = Flask(__name__)
+#cross domain requests
+CORS(app)
+
+XCEPTION_INPUT_SHAPE_SIZE = 299
+CLASSES = ['NotMelanoma', 'Melanoma']
+
+@app.route('/')
+@cross_origin()
+def index():
+    return "Working"
+
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    request_body = request.get_json()
+    image_base64 = request_body['imagebase64']
+    image_path = './test.jpeg'
+
+    if os.path.exists(image_path):
+        os.remove(image_path)
+
+    with Image.open(BytesIO(base64.decodebytes(bytes(image_base64, "utf-8")))) as img:
+        img.save(image_path, 'jpeg')
+
+    input_image = tf.keras.preprocessing.image.load_img(image_path)
+
+    input_image = tf.keras.preprocessing.image.img_to_array(input_image)
+    input_image = tf.image.resize(input_image,
+                              [XCEPTION_INPUT_SHAPE_SIZE,XCEPTION_INPUT_SHAPE_SIZE])
+    input_image = tf.keras.applications.xception.preprocess_input(input_image)
+    input_image = np.expand_dims(input_image, axis=0)
+
+    # this line is added because of a bug in tf_serving < 1.11
+    input_image = input_image.astype('float16')
+
+    model = tf.keras.models.load_model("./cnn-models/xception/1/")
+    prediction_res = model.predict(input_image.tolist())
+    print("prediction shape:", prediction_res)
+
+    prediction = prediction_res[0][0]
+    class_name = CLASSES[int(prediction > 0.5)]
+    percentage = prediction * 100
+
+    response_body = {}
+    response_body["prediction"] = class_name
+    response_body["percent"] = percentage
+
+    return jsonify({"status": 200, "data": response_body})
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    #serve(app, host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
