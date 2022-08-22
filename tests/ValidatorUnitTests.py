fe0d094f8b2286257c74bf88860ff9c0630fde55
Yauhen Bichel <5603242+YauhenBichel@users.noreply.github.com>
2022-08-22 23:06:16 +0100

    added tests

--- tests/ValidatorUnitTests.py
+++ tests/ValidatorUnitTests.py
@@ -5,7 +5,6 @@ from ml_model_serving.Validator import Validator
 
 class ValidatorTestCase(unittest.TestCase):
     def test_validator_validate_prediction_id_when_empty_then_false(self):
-
         #arrange
         expectedResult = False
         validator = Validator()
@@ -17,13 +16,56 @@ class ValidatorTestCase(unittest.TestCase):
         self.assertEqual(expectedResult, actualResult)
 
     def test_validator_validate_prediction_id_when_none_then_false(self):
-
         #arrange
         expectedResult = False
         validator = Validator()
 
         #act
         actualResult = validator.validate_prediction_id(None)
+
+        #assert
+        self.assertEqual(expectedResult, actualResult)
+
+    def test_validator_validate_prediction_id_when_uuid_then_true(self):
+        #arrange
+        expectedResult = True
+        validator = Validator()
+
+        #act
+        actualResult = validator.validate_prediction_id("8b593281-01c1-4f23-8ffe-7589fdd45e63")
+
+        #assert
+        self.assertEqual(expectedResult, actualResult)
+
+    def test_validator_validate_image_base64_when_empty_then_false(self):
+        #arrange
+        expectedResult = False
+        validator = Validator()
+
+        #act
+        actualResult = validator.validate_image_base64("")
+
+        #assert
+        self.assertEqual(expectedResult, actualResult)
+
+    def test_validator_validate_image_base64_when_none_then_false(self):
+        #arrange
+        expectedResult = False
+        validator = Validator()
+
+        #act
+        actualResult = validator.validate_image_base64(None)
+
+        #assert
+        self.assertEqual(expectedResult, actualResult)
+
+    def test_validator_validate_image_base64_when_image_then_true(self):
+        #arrange
+        expectedResult = True
+        validator = Validator()
+
+        #act
+        actualResult = validator.validate_image_base64("image")
 
         #assert
         self.assertEqual(expectedResult, actualResult)
