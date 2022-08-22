d552d4cebf41c8eb573a32aa4dd7ea8ff4eb40a7
Yauhen Bichel <5603242+YauhenBichel@users.noreply.github.com>
2022-08-22 23:00:15 +0100

    added tests

--- tests/ValidatorUnitTests.py
+++ tests/ValidatorUnitTests.py
@@ -6,20 +6,26 @@ from ml_model_serving.Validator import Validator
 class ValidatorTestCase(unittest.TestCase):
     def test_validator_validate_prediction_id_when_empty_then_false(self):
 
+        #arrange
         expectedResult = False
         validator = Validator()
 
+        #act
         actualResult = validator.validate_prediction_id("")
 
+        #assert
         self.assertEqual(expectedResult, actualResult)
 
     def test_validator_validate_prediction_id_when_none_then_false(self):
 
+        #arrange
         expectedResult = False
         validator = Validator()
 
+        #act
         actualResult = validator.validate_prediction_id(None)
 
+        #assert
         self.assertEqual(expectedResult, actualResult)
 
 
